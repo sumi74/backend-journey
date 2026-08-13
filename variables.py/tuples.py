36f46818dac5fd64176fe0155514916765b915b9
sumi74 <sundus.ahmed421@gmail.com>
2026-08-13 23:01:10 +0300

    tuples.py

---
+++ variables.py/tuples.py
@@ -0,0 +1,11 @@
+fruits = ("apple", "banana", "orange")
+
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+print(len(fruits))
+print("banana" in fruits)
+print(fruits.count("apple"))
+print(fruits.count("orange"))

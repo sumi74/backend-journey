ba9b75c10468679cf8c8f548002dd014f07ccb2f
sumi74 <sundus.ahmed421@gmail.com>
2026-08-13 23:21:13 +0300

    sets.py

---
+++ variables.py/sets.py
@@ -0,0 +1,14 @@
+fruits = {"apple", "banana", "orange", "apple"}
+
+print(fruits)
+print(len(fruits))
+fruits.add("mango")
+print(fruits)
+fruits.remove("banana")
+print(fruits)
+fruits.discard("apple")
+print(fruits)
+fruits.pop()
+print(fruits)
+fruits.clear()
+print(fruits)

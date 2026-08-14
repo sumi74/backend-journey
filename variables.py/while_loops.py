9b500a4ccd7d2e3fc8b9fc1ec8e5f06c73f8d984
sumi74 <sundus.ahmed421@gmail.com>
2026-08-14 23:52:46 +0300

    while_loops.py

---
+++ variables.py/while_loops.py
@@ -0,0 +1,17 @@
+count = 1
+
+while count <= 5:
+    print(count)
+    count = count + 1
+#
+count = 5
+
+while count <= 5:
+    print(count)
+    count = count + 1
+#
+count = 5
+
+while count <= 1:
+    print(count)
+    count = count - 1

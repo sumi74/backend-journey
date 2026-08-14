84faba9fbe3b262192d344531ef8ab6c05efb0fd
sumi74 <sundus.ahmed421@gmail.com>
2026-08-15 01:03:44 +0300

    while.loops.py

--- variables.py/while_loops.py
+++ variables.py/while_loops.py
@@ -15,3 +15,73 @@ count = 5
 while count <= 1:
     print(count)
     count = count - 1
+
+#
+
+password = ""
+
+while password != "python":
+    password = input("enter password: ")
+
+print("correct!")
+
+#
+count = 1
+
+while count <= 10:
+    print(count)
+
+    if count == 5:
+        break
+
+    count = count + 1
+
+#
+count =1
+while count <= 10:  
+    print(count)
+    if count == 7:
+        break
+
+    count = count + 1
+#
+count = 1
+
+while count <= 10:
+    if count ==4:
+        break
+
+    print(count)
+    count = count + 1
+#
+count = 1
+
+while count <= 5:
+    count = count + 1
+
+    if count == 4:
+        break
+
+    print(count)
+#
+count = 0
+
+while count < 5:
+    count = count + 1
+
+    if count == 3:
+        continue
+
+    print(count)
+
+#
+count = 0
+
+while count < 6:
+    count = count + 1
+
+    if count ==2:
+        continue
+
+    print(count)
+    

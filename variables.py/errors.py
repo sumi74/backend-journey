a7b6bc56092219dcf34f4b621cd5f97c315bdb55
sumi74 <sundus.ahmed421@gmail.com>
2026-08-15 19:30:41 +0300

    errors.py

---
+++ variables.py/errors.py
@@ -0,0 +1,10 @@
+try:
+    number = int(input("enter a number"))
+    print(10 / number)
+
+except ValueError:
+     print("please enter a number.")
+
+except ZeroDivisionError:
+     print("you cannot divide by zero.")
+     

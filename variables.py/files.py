b8072e7b0f374e6ade0ca5aac79197900c776b44
sumi74 <sundus.ahmed421@gmail.com>
2026-08-16 21:42:20 +0300

    files.py

--- variables.py/files.py
+++ variables.py/files.py
@@ -1,9 +1,9 @@
-file = open("data.txt", "w")
-file.write("hello, backend developer!")
-file.close()
-#
+with open("data.txt", "w") as file:
+    file.write("i am learning python")
 
-file = open("data.txt", "r")
-content = file.read()
-print(content)
-file.close()
+with open("data.txt", "a") as file:
+    file.write("\nl am becoming backend developer")
+
+with open("data.txt", "r") as file:
+    content = file.read()
+    print(content)

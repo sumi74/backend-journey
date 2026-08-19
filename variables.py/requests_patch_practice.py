54e62a767c731dd80a1c9f943da452282d8fda99
sumi74 <sundus.ahmed421@gmail.com>
2026-08-19 23:44:09 +0300

    requests_patch_practice.py

---
+++ variables.py/requests_patch_practice.py
@@ -0,0 +1,12 @@
+import requests
+data = {
+    "title": "backend developer"
+}
+
+response = requests.patch(
+    "https://jsonplaceholder.typicode.com/posts/1",
+    json=data 
+)
+
+print("status:", response.status_code)
+print("updated:", response.json())

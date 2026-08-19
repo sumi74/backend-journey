2fec968486874276ae39ac7b2856de5a5ca683bb
sumi74 <sundus.ahmed421@gmail.com>
2026-08-19 23:16:30 +0300

    requests_json_practice

---
+++ variables.py/requests_json_practice.py
@@ -0,0 +1,12 @@
+import requests
+
+response = requests.get(
+    "https://jsonplaceholder.typicode.com/users/1"
+)
+print("status:", response.status_code)
+
+data = response.json()
+
+print("name", data["name"])
+print("email:", data["email"])
+print("city:", data["address"]["city"])

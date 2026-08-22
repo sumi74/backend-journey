fb9f937ec971a6e7487ea7aa0c1370a8dec0c800
sumi74 <sundus.ahmed421@gmail.com>
2026-08-22 22:00:40 +0300

    api_user.py

---
+++ variables.py/api_user.py
@@ -0,0 +1,13 @@
+import requests
+
+response = requests.get(
+    "https://jsonplaceholder.typicode.com/users/1"
+)
+
+print("status:", response.status_code)
+
+data = response.json()
+
+print("name", data["name"])
+print("email", data["email"])
+print("city", data["address"]["city"])

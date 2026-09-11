886f8607cb514dfe568c822b6e99787fa5eddb31
unknown <sundus.ahmed421@gmail.com>
2026-09-11 12:57:56 -0700

    student_managment.sql

--- variables.py/sql/student_managment.sql
+++ variables.py/sql/student_managment.sql
@@ -36,5 +36,115 @@ VALUES
 
 SELECT * FROM students;
 
+INSERT INTO courses (name, teacher, fee)
+VALUES
+('python', 'mr.john', 300),
+('sql', 'ms.sarah', 250),
+('django','mr.david', 400),
+('web development', 'ms.mary', 350);
+
+SELECT * FROM courses;
 
 
+INSERT INTO enrollments (student_id, course_id, grade)
+VALUES
+(1, 1, 85),
+(1, 2, 90),
+(2, 1, 72),
+(2, 3, 80),
+(3, 2, 88),
+(3, 4, 91),
+(4, 1, 95),
+(4, 3, 87),
+(5, 2, 76);
+
+SELECT * FROM enrollments;
+
+
+SELECT students.name
+FROM students;
+
+
+sELECT students.name
+FROM students
+JOIN enrollments
+ON students.id = enrollments.student_id;
+
+SELECT 
+    students.name,
+    courses.name
+FROM students
+JOIN enrollments
+ON students.id = enrollments.student_id
+JOIN courses
+ON enrollments.course_id = courses.id;
+
+
+SELECT
+    students.name,
+    courses.name,
+    enrollments.grade
+FROM students
+JOIN enrollments
+    ON students.id = enrollments.student_id
+JOIN courses
+    ON enrollments.course_id = courses.id;
+
+
+SELECT 
+    students.name,
+    courses.name,
+    enrollments.grade
+FROM students
+JOIN enrollments
+    ON students.id = enrollments.student_id
+JOIN courses
+    ON enrollments.course_id = courses.id
+WHERE enrollments.grade > 85;
+
+
+SELECT 
+    courses.name,
+    avg(enrollments.grade) AS average_grade
+FROM courses
+JOIN enrollments
+    ON courses.id = enrollments.course_id
+GROUP BY courses.id;
+
+SELECT 
+    courses.name,
+    count(enrollments.student_id) AS student_count
+FROM courses
+JOIN enrollments
+    ON courses.id = enrollments.course_id
+GROUP BY courses.id;
+
+
+SELECT 
+    courses.name,
+    count(enrollments.student_id) AS student_count
+FROM courses
+JOIN enrollments
+    ON courses.id = enrollments.course_id
+GROUP BY courses.id
+HAVING count(enrollments.student_id) > 2;
+
+SELECT
+    students.name,
+    enrollments.grade
+FROM students
+JOIN enrollments
+    ON students.id = enrollments.student_id
+WHERE enrollments.grade = (SELECT max (grade) FROM enrollments);
+
+SELECT 
+    students.name,
+    sum(courses.fee) AS total_paid
+FROM students
+JOIN enrollments
+    ON students.id = enrollments.student_id
+JOIN courses
+    ON enrollments.course_id = courses.id
+JOIN courses
+    ON enrollments.course_id = courses.id
+GROUP BY students.id, students.name;
